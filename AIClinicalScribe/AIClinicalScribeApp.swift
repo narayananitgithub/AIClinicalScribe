db57c13a483f2976edc1bd1704ab915a74ee3214
Narayanasamy <narayanasamyamirthalingam@gmail.com>
2025-07-11 14:16:40 +0530

    Initial Commit

---
+++ AIClinicalScribe/AIClinicalScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIClinicalScribeApp.swift
+//  AIClinicalScribe
+//
+//  Created by Narayanasamy on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AIClinicalScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIClinicalScribeTests/AIClinicalScribeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIClinicalScribeTests.swift
+//  AIClinicalScribeTests
+//
+//  Created by Narayanasamy on 11/07/25.
+//
+
+import Testing
+@testable import AIClinicalScribe
+
+struct AIClinicalScribeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
